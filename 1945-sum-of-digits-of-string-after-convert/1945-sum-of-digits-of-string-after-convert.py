725fdd87036b0cff26d1e3bbcbbb3a9f9c1027eb
Shukrullo <nazirjonovsh2000@gmail.com>
2022-05-25 17:17:06 +0900

    Time: 89 ms (5.44%), Space: 13.9 MB (72.16%) - LeetHub

--- 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.py
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.py
@@ -3,10 +3,6 @@ class Solution:
     def getLucky(self, s: str, k: int) -> int:
         pos=[string.ascii_lowercase.index(list(s)[i])+1 for i in range(len(list(s)))]
         pos_string=''.join(map(str,pos))
-        while(k!=0):
-            digit=[i for i in pos_string]
-            sumdigit=sum(map(int,digit))
-            k=k-1
-            pos_string=list(str(sumdigit))
-        return sumdigit
-            
+        for i in range(k):
+            pos_string = str(sum(map(int, pos_string)))
+        return pos_string
