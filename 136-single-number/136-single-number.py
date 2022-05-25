f78c7ef403ea105e8ae6de5c5ffd2b1460494edc
Shukrullo <nazirjonovsh2000@gmail.com>
2022-05-25 11:15:03 +0900

    Time: 146 ms (77.03%), Space: 17.3 MB (5.64%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,6 +1,4 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        result=0
-        for i in range(len(nums)):
-            result^=nums[i]
-        return result
+        new_set=set(nums)
+        return (sum(new_set)*2-sum(nums))
