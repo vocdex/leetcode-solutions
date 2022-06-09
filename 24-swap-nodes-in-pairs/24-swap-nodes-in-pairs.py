6bb2b6c07828f74ca50912545406af12f783f763
Shukrullo <nazirjonovsh2000@gmail.com>
2022-06-09 11:09:14 +0900

    Time: 36 ms (75.34%), Space: 13.8 MB (97.01%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
@@ -9,19 +9,19 @@ class Solution:
             return None
         if head.next is None:
             return head
+        # dummy is the node that points to head
         dummy=ListNode(0,head)
         prev,curr=dummy,head
         while curr and curr.next:
-            # save ptrs
-            nextPairStart=curr.next.next
-            second=curr.next
+            nextPairstart=curr.next.next # starting point for the next pair
+            second=curr.next # declaring the second node in swapping pair
             # reverse this pair
             second.next=curr
-            curr.next=nextPairStart
-            prev.next=second
-            
-            #update ptrs
-            prev=curr
-            curr=nextPairStart
+            curr.next=nextPairstart
+            prev.next=second # changing the prev point to the original second node which is now the #first node in the new pair
+            #update pointers
+            prev=curr # this node now points to the node before the starting node of the next pair
+            curr=nextPairstart # curr is the starting node of the next pair
         return dummy.next
-        
+            
+            
