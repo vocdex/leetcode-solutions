5710a380194e7e6b2e5f7c5cf03f6dea3ea134f4
Shukrullo Nazirjonov <nazirjonovsh2000@gmail.com>
2022-06-21 21:52:52 +0900

    Time: 305 ms (9.17%), Space: 18 MB (83.99%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.py
@@ -0,0 +1,10 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        out = []
+        # key function is used to access the first elements of each interval in the list
+        for i in sorted(intervals, key=lambda i: i[0]):
+            if out and i[0] <= out[-1][1]:
+                out[-1][1] = max(out[-1][1], i[1])
+            else:
+                out += [i]
+        return out
